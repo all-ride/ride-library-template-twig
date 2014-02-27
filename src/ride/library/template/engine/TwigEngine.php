c768ca49435d3717753e25149908c054cff8e6c5
Joris Vandeweerd <joris@statik.be>
2014-02-27 18:30:33 +0100

    initial commit

---
+++ src/ride/library/template/engine/TwigEngine.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace ride\library\template\engine;
+
+use ride\library\template\theme\ThemeModel;
+use ride\library\template\Template;
+use ride\library\template\ThemedTemplate;
+use ride\library\system\file\File;
+
+use \Twig_Environment;
+use \Twig_Extension;
+
+/**
+ * Twig implementation for a template engine
+ */
+class TwigEngine extends AbstractEngine {
+
+    /**
+     * Name of this engine
+     * @var string
+     */
+    const NAME = 'twig';
+
+    /**
+     * Extension for resources of this engine
+     * @var string
+     */
+    const EXTENSION = 'twig';
+
+    /**
+     * Instance of the Twig engine
+     * @var Twig_Environment
+     */
+    protected $twig;
+
+    /**
+     * Implementation of the resource handler
+     * @var TwigLoader
+     */
+    protected $loader;
+
+    /**
+     * Constructs a new Smarty template engine
+     * @param ride\library\template\TwigLoader $loader Resource handler for
+     * the template engine
+     * @param ride\library\system\file\File $compileDirectory Directory for
+     * the compiled templates
+     * @return null
+     */
+    public function __construct(TwigLoader $loader, File $compileDirectory, ThemeModel $themeModel) {
+        $compileDirectory->create();
+
+        $this->loader = $loader;
+        $this->twig = new Twig_Environment($this->loader, array(
+            'cache' => $compileDirectory->getPath(),
+            'auto_reload' => true,
+        ));
+
+        $this->setThemeModel($themeModel);
+    }
+
+    /**
+     * Gets the instance of Twig
+     * @return Twig_Environment
+     */
+    public function getTwig() {
+        return $this->twig;
+    }
+
+    /**
+     * Adds an extension to the Twig engine
+     * @param Twig_Extension $extension
+     * @return null
+     */
+    public function addExtension(Twig_Extension $extension) {
+        $this->twig->addExtension($extension);
+    }
+
+    /**
+     * Renders a template
+     * @param ride\library\template\Template $template Template to render
+     * @return string Rendered template
+     * @throws ride\library\template\exception\ResourceNotSetException when
+     * no template resource was set to the template
+     * @throws ride\library\template\exception\ResourceNotFoundException when
+     * the template resource could not be found by the engine
+     */
+    public function render(Template $template) {
+        $resource = $template->getResource();
+        if (!$resource) {
+            throw new ResourceNotSetException();
+        }
+
+        $this->preProcess($template);
+
+        try {
+            $output = $this->twig->render($resource, $template->getVariables());
+
+            $exception = null;
+        } catch (Exception $e) {
+            ob_get_clean();
+
+            $exception = $e;
+        }
+
+        $this->postProcess();
+
+        if ($exception) {
+            throw $exception;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Gets the template resource
+     * @param ride\library\template\Template $template Template to get the
+     * resource of
+     * @return string Absolute path of the template resource
+     * @throws ride\library\template\exception\ResourceNotSetException when
+     * no template was set to the template
+     * @throws ride\library\template\exception\ResourceNotFoundException when
+     * the template could not be found by the engine
+     */
+    public function getFile(Template $template) {
+        $resource = $template->getResource();
+        if (!$resource) {
+            throw new ResourceNotSetException();
+        }
+
+        $this->preProcess($template);
+
+        $file = $this->loader->getFile($resource);
+
+        return $file->getAbsolutePath();
+    }
+
+    protected function preProcess(Template $template) {
+        if (!$template instanceof ThemedTemplate) {
+            return;
+        }
+
+        $themeHierarchy = $this->getTheme($template);
+
+        $this->loader->setThemes($themeHierarchy);
+
+        $templateId = $template->getResourceId();
+        if ($templateId) {
+            $this->loader->setTemplateId($templateId);
+        }
+    }
+
+    protected function postProcess() {
+        $this->loader->setThemes(null);
+        $this->loader->setTemplateId(null);
+    }
+
+}
---
+++ src/ride/library/template/engine/TwigLoader.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace ride\library\template\engine;
+
+use ride\library\system\file\browser\FileBrowser;
+use ride\library\template\exception\ResourceNotFoundException;
+
+use \Twig_LoaderInterface;
+
+/**
+ * Default resource handler for Smarty according to the Zibo standards
+ */
+class TwigLoader implements Twig_LoaderInterface {
+
+    /**
+     * File browser to lookup the templates
+     * @var ride\library\system\file\browser\FileBrowser;
+     */
+    protected $fileBrowser;
+
+    /**
+     * Path for the file browser
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Themes to use when looking for resources
+     * @var array
+     */
+    protected $themes;
+
+    /**
+     * Id of the template
+     * @var string
+     */
+    protected $templateId;
+
+    /**
+     * Constructs a new resource handler
+     * @param ride\library\system\file\browser\FileBrowser $fileBrowser File
+     * browser to lookup the templates
+     * @return null
+     */
+    public function __construct(FileBrowser $fileBrowser, $path = null) {
+        $this->fileBrowser = $fileBrowser;
+
+        $this->setPath($path);
+    }
+
+    /**
+     * Sets the path for the file browser
+     * @param string $path
+     * @return null
+     * @throws ride\library\template\exception\TemplateException when the
+     * provided path is invalid or empty
+     */
+    public function setPath($path) {
+        if ($path !== null && (!is_string($path) || !$path)) {
+            throw new TemplateException('Could not set the path for the file browser: provided path is empty or invalid');
+        }
+
+        $this->path = $path;
+    }
+
+    /**
+     * Sets the themes used for looking the template resource
+     * @param array $themes Array with the name of the themes as key
+     * @return null
+     * @throws ride\library\template\exception\TemplateException when the
+     * provided theme is invalid or empty
+     */
+    public function setThemes(array $themes = null) {
+        $this->themes = $themes;
+    }
+
+    /**
+     * Sets the id of the template. Don't forget to set compile_id on the
+     * Smarty engine itself.
+     * @param string $templateId Id of the template
+     * @return null
+     */
+    public function setTemplateId($templateId) {
+        $this->templateId = $templateId;
+    }
+
+    /**
+     * Gets the source code of a template, given its name.
+     * @param  string $name string The name of the template to load
+     * @return string The template source code
+     */
+    public function getSource($name) {
+        $templateFile = $this->getFile($name);
+
+        return $templateFile->read();
+    }
+
+    /**
+     * Gets the cache key to use for the cache for a given template name.
+     * @param  string $name string The name of the template to load
+     * @return string The cache key
+     */
+    public function getCacheKey($name) {
+        $key = $name;
+
+        if ($this->themes) {
+            reset($this->themes);
+            $theme = key($this->themes);
+
+            $key = $theme . '-' . $key;
+        }
+
+        if ($this->templateId) {
+            $key .= $this->templateId;
+        }
+
+        return $key;
+    }
+
+    /**
+     * Returns true if the template is still fresh.
+     *
+     * @param string    $name The template name
+     * @param timestamp $time The last modification time of the cached template
+     */
+    public function isFresh($name, $time) {
+        $templateFile = $this->getFile($name);
+
+        return $templateFile->getModificationTime() <= $time;
+    }
+
+    /**
+     * Get the source file of a template
+     * @param string $name Relative path of the template to the view folder
+     * without the extension
+     * @return ride\library\system\file\File instance of a File if the source
+     * is found, null otherwise
+     */
+    public function getFile($name) {
+        $file = null;
+
+        if ($this->themes) {
+            foreach ($this->themes as $theme => $null) {
+                try {
+                    $file = $this->getThemeFile($name, $theme);
+
+                    break;
+                } catch (ResourceNotFoundException $exception) {
+                    $file = null;
+                }
+            }
+        }
+
+        if (!$file) {
+            $file = $this->getThemeFile($name);
+        }
+
+        return $file;
+    }
+
+    /**
+     * Gets the source file of a template
+     * @param string $name Relative path of the template to the view folder
+     * without the extension
+     * @return ride\library\system\file\File Instance of a File if the source
+     * is found, null otherwise
+     */
+    protected function getThemeFile($name, $theme = null) {
+        $path = '';
+        if ($theme) {
+            $path = $theme . '/';
+        }
+
+        if ($this->path) {
+            $path = $this->path . '/' . $path;
+        }
+
+        if ($path) {
+            $path = rtrim($path, '/') . '/';
+        }
+
+        $file = null;
+
+        if ($this->templateId) {
+            $fileName = $path . $name . '.' . $this->templateId . '.' . TwigEngine::EXTENSION;
+
+            $file = $this->fileBrowser->getFile($fileName);
+        }
+
+        if (!$file) {
+            $fileName = $path . $name . '.' . TwigEngine::EXTENSION;
+
+            $file = $this->fileBrowser->getFile($fileName);
+        }
+
+        if (!$file) {
+            throw new ResourceNotFoundException($fileName);
+        }
+
+        return $file;
+    }
+
+}
